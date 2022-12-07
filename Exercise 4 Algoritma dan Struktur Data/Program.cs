e89cc57d3e5373c31b2044c3ef69a73c4a0c9a45
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-12-07 15:44:57 +0700

    membuat function empty

--- Exercise 4 Algoritma dan Struktur Data/Program.cs
+++ Exercise 4 Algoritma dan Struktur Data/Program.cs
@@ -22,7 +22,23 @@ namespace Exercise_4
             top = null;
         }
 
+        bool empty()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
 
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            
+        }
     }
 }
 
